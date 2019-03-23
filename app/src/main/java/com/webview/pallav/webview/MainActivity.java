07f72e9cf5566fee0daa3aa2ecdd6b65840e0ee4
Pallav Shah <pallavmac@Pallavs-MacBook-Pro.local>
2019-03-23 19:33:54 -0400

    Initial Commit WV

---
+++ app/src/main/java/com/webview/pallav/webview/MainActivity.java
@@ -0,0 +1,42 @@
+package com.webview.pallav.webview;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+public class MainActivity extends Activity {
+
+    private WebView webView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.webView = (WebView) findViewById(R.id.activity_main_webview);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        WebViewClientImpl webViewClient = new WebViewClientImpl(this);
+        webView.setWebViewClient(webViewClient);
+
+        webView.loadUrl("http://www.journaldev.com");
+        //webView.loadData("<html><body>Hello, world!</body></html>", "text/html", "UTF-8");
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && this.webView.canGoBack()) {
+            this.webView.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+}
